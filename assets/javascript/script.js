df2e7931c19aec6bc4566743a00648921e665fe4
Vanessa G <vlopez_jv@hotmail.com>
2021-04-10 14:01:40 -0700

    fixed answer null

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,111 +1,172 @@
-//select all elements by using a get element method. 
-var questionEl = document.getElementById("question");
-var answerEl = document.getElementById("answer");
-var scoreEl = document.getElementById("score");
-var timerEl = document.getElementById("timer");
-//start
-var start = document.getElementById("start");
-//quiz
-var quiz = document.getElementById("quiz");
-//timer
-var counter = document.getElementById("counter");
-var countdown = document.getElementById("countdown");
-var progress = document.getElementById("progress");
-
-
-//CREATE quizQUESTIONS 
-
-//use array
-
-let quizQuestions = [
-    {
-        question: "this is question 1?",
-        choiceA: "answer 1",
-        choiceB: "answer 2",
-        choiceC: "answer 3",
-        choiceD: "answer 4",
-        choiceE: "answer 5",
-        correct: "A"
-    },
-    {
-        question: "this is question 2?",
-        choiceA: "answer 1",
-        choiceB: "answer 2",
-        choiceC: "answer 3",
-        choiceD: "answer 4",
-        choiceE: "answer 5",
-        correct: "B"
-    },
-    {
-        question: "this is question 3?",
-        choiceA: "answer 1",
-        choiceB: "answer 2",
-        choiceC: "answer 3",
-        choiceD: "answer 4",
-        choiceE: "answer 5",
-        correct: "C"
-    },
-    {
-        question: "this is question 4?",
-        choiceA: "answer 1",
-        choiceB: "answer 2",
-        choiceC: "answer 3",
-        choiceD: "answer 4",
-        choiceE: "answer 5",
-        correct: "D"
-    },
-    {
-        question: "this is question 5?",
-        choiceA: "answer 1",
-        choiceB: "answer 2",
-        choiceC: "answer 3",
-        choiceD: "answer 4",
-        choiceE: "answer 5",
-        correct: "E"
-    }
-]
-
-//CREATE variables for quizQuestions
-var lastQuestion = quizQuestions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-let timer;
+//create global let values for questions, answers and score, progress, timer
+//use document.getelementbyId()
+let questionElement = document.getElementById("questions");
+let answerElement = document.getElementById("answer-buttons");
+let scoreElement = document.getElementById("score-results");
+let progressElement = document.getElementById("progress");
+let timerElement = document.getElementById("timer");
+//
 let answer;
-let score = 0;
 
-//render question
-function questionEl() {
-    let q = questions[runningQuestion];
+//set the timer 
+let timer = 60;
 
-    question.innerHTML = "<p>" + q.question + "</p>";
-    qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
+//set question begining 
+let questionNum = -1; //this is to keep from skipping a question unlike if you set it at 0
 
-start.addEventListener("click", startQuiz);
-//start quiz 
+
+//create the questions array
+// let name = [ { q: "q", a:["a","b","c"], correctA: "a"}, repeat}];
+
+let quizquizQu = [
+    {
+        question: "this is question 1?", answer: [
+            " 1",
+            " 2",
+            " 3",
+            " 4",
+        ],
+        correct: "1"
+    },
+    {
+        question: "this is question 2?", answer: [
+            " 1",
+            " 2",
+            " 3",
+            " 4",
+        ],
+        correct: "2"
+    },
+    {
+        question: "this is question 3?", answer: [
+            " 1",
+            " 2",
+            " 3",
+            " 4",
+        ],
+        correct: "3"
+    },
+    {
+        question: "this is question 4?", answer: [
+            " 1",
+            " 2",
+            " 3",
+            " 4",
+        ],
+        correct: "4"
+    },
+
+]
+ 
+//now create a function to start the quiz ie: func name()
+//use getelementbyId to pull the element then 
+//hide the first intro card after starting the quiz
+//then start the timer as soon as the game starts
+//makesure the questions are being hidden
+let startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startQuiz)
+
 function startQuiz() {
+    console.log('started');
+    document.getElementById('intro_container').classList.add("hide");
+    document.getElementById('quiz').classList.remove("hide")
 
-    document.getElementById("start").classList.add("hidden");
-    document.getElementById("quiz").classList.remove("hidden");
+    //prompt the timer function and the quizquestions
+
+    countdown();
+    //timer
+    quizQuestions();
+    //prompt questions
+};
+
+//create the start timer function here
+
+let countdown = function () {
+    let countdown = setInterval(function () {
+        timer--;
+        timerElement.textContent = "Time: " + timer + " seconds remaining";
+
+        if (timer === 0 || questionNum === quizQuestionsArray.length) {
+            clearInterval(timer);
+            setTimeout(showScore, 100);
+        
+        }
+    }, 1000);
+};
+
+let Score_points = 100
+ let max_questions =4
+ let lastQuestion = quizQuestionsArray.length - 1;
+ //create the quizQuestion function
+
+// This sets the questions and also buttons containing our answers
+function quizQuestions() {
     
-  };
+    questionNum++
+
+    // This presents our question as a textContent to the user.
+    questionElement.textContent = quizQuestionsArray[questionNum].question
+        
+    answerElement.innerHTML = "";
+
+    let answers = quizQuestionsArray[questionNum].answers;
+
+    answer = quizQuestionsArray[questionNum].correct;
+
+    for (let i = 0; i < answers.length; i++){
+        let userChoice = document.createElement("button");
+
+        userChoice.textContent = answers[i];
+
+        nextBtn = selectionsEl.appendChild(userChoice);
+    }
+};
+
+//Answer Choice Buttons
+answerElement.addEventListener("click", function(event) {
+    let progressEl = document.getElementsByClassName("progress") [0]
+
+    if (answer === event.target.textContent) {
+        // Let user know answer is correct
+        progressEl.textContent = "Correct Answer!"
+        // Sets the amount of time message is displayed
+        setTimeout(hideProgress, 3000);
+        showProgress();
+    }
+    else { 
+        // Let user know answer is incorrect
+        progressEl.textContent = "Incorrect Answer!";
+        // Sets the amount of time message is displayed
+        setTimeout(hideProgress, 3000);
+        // Subtract ten seconds from time left
+        timeLeft = timeLeft - 10;
+        showProgress();
+    }
+    quizQuestions();
+});
 
 
-//render progress
 
-//render counter
-// Timer that counts down from 60
-function countdown() {
-    var timeLeft = 60;}
 
-//check answer
 
-//answer is correct
 
-//answer is wrong
 
-//score render function//
 
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+// let timeLeftDisplay = document.getElementById('#timeleft')
+// let timeLeft = 60
+
+// function countdown(){
+//     setInterval(function(){
+//         if(timeLeft <=0){
+//             clearInterval(timeLeft= 0)
+//         }
+//         timeLeftDisplay.innerHTML = timeLeft,
+//         timeLeft = -1
+//     }, 1000)
+// }
+//     startButton.addEventListener('click', countdown) 
+// })
